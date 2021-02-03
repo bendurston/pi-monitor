6b028d5ba20bbe2da33ceb7497e3b483b30a65a5
Ben Durston <bengdurston@gmail.com>
2021-02-03 08:29:37 -0500

    Created home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return(
+        <div>
+            <h1 className="bg-gray-200">Hello From Home!</h1>
+        </div>
+    )
+
+}
